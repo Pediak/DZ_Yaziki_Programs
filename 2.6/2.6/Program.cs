ec60ea32f9a4a46d1b2f9df017e106cc84ae0dea
Pediak <akovmari@gmail.com>
2026-03-30 13:19:19 +0500

    Add files via upload

---
+++ 2.6/2.6/Program.cs
@@ -0,0 +1,56 @@
+﻿//Пользователь вводит любой символ (переменная char). Необходимо проверить, является ли символ русской буквой и если да,
+//то определить гласная она или согласная. Вывести сообщение на экран.
+//Если введена не русская буква, то вывести сообщение "Error".
+
+
+
+public class Proverka
+{
+    private char Bukva;
+    
+    public Proverka(char Bucva)
+    {
+        Bukva = Bucva; 
+    }
+
+    public string TypE()
+    {
+        string rez = "";
+        Bukva = char.ToLower(Bukva);
+
+        if ((Bukva == 'ё') || (Bukva >= 'а' && Bukva <='я'))
+        {
+            rez += "буква Русская";
+            char ch = char.ToLower(Bukva);
+
+            if (ch == 'а' || ch == 'е' || ch == 'ё' || ch == 'и' || ch == 'о' ||
+                ch == 'у' || ch == 'ы' || ch == 'э' || ch == 'ю' || ch == 'я')
+            {
+                rez += " и гласная";
+            }
+            else
+            {
+                rez += " и согласная";
+            }
+        }
+        else
+        {
+            return "Error";
+        }
+        return rez;
+    }
+}
+
+
+
+class Prodramm
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите букву: ");
+        char ch = char.Parse(Console.ReadLine());
+
+        Proverka ch1 = new Proverka(ch);
+        Console.WriteLine(ch1.TypE());
+    }
+}
---
+++ 2.7/2.7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.Design;
+using static System.Math;
+
+class Cupe
+{
+    private int Num;
+    public Cupe(int num)
+    {
+        Num = num;
+    }
+
+    public string Bilet()
+    {
+        string bilet = "";
+        if (Num <= 36)
+        {
+            bilet += Convert.ToString((Num + 4 - 1)/4);
+            bilet += "к";
+
+            if(Num % 2 == 0)
+            {
+                bilet += "в";
+            }
+
+            else
+            {
+                bilet += "н";
+            }
+        }
+
+        else if (Num > 36 && Num <= 54)
+        {
+            bilet += Convert.ToString(10 - ((Num - 36 + 2 - 1) / 2));
+            bilet += "б";
+
+            if (Num % 2 == 0)
+            {
+                bilet += "в";
+            }
+
+            else
+            {
+                bilet += "н";
+            }
+        }
+
+        else
+        {
+            return "не существующий номер";
+        }
+
+        return bilet;
+    }
+}
+
+
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номер места: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Cupe bilet1 = new Cupe(num);
+        Console.WriteLine($"билет в формате NBS {bilet1.Bilet()}");
+    }
+}
